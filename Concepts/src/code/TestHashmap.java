b75ca9b6ae0d91239be684198f94e8967b9f94ed
deep.gupta <deep.gupta@com>
2019-01-09 20:10:51 +0530

    updated file

--- Concepts/src/code/TestHashmap.java
+++ Concepts/src/code/TestHashmap.java
@@ -7,10 +7,16 @@ public class TestHashmap {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr= {5,3,2,1};
-		System.out.println(time(arr));
-		System.out.println(ans(arr));
+		int[] standard= {2,3,5,9};
+		HashMap<Integer, Integer> Result =time(arr);
+		
+		for (int i = 0; i < standard.length; i++) {
+			Result=ans(Result,standard[i]);
+		}
 		
 	}
+
+		
 
 		public static HashMap<Integer, Integer> time(int[] arr) {
 			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -22,10 +28,16 @@ public class TestHashmap {
 					map.put(arr[i], 1);
 			  
 			}
+			System.out.println(map);
 			return map;
 		}
 		
-		public static HashMap<Integer, Integer> name() {
+		private static HashMap<Integer, Integer> ans(HashMap<Integer, Integer> time, int limit) {
+			for (int i = limit; i > 0; i--) {
+				
+			}
+ 
 			
+			return time;
 		}
 }
