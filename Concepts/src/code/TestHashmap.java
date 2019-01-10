7b948e4f02e18a506dee994029baf0614e0b2250
deep.gupta <deep@com>
2019-01-10 20:07:44 +0530

    update

--- Concepts/src/code/TestHashmap.java
+++ Concepts/src/code/TestHashmap.java
@@ -2,42 +2,48 @@ package code;
 
 import java.util.HashMap;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class TestHashmap {
 
+	static String result1 = "";
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] arr= {5,3,2,1};
-		int[] standard= {2,3,5,9};
-		HashMap<Integer, Integer> Result =time(arr);
-		
+
+		int[] arr = { 5, 3, 7, 1 };
+		int[] standard = { 2, 3, 5, 9 };
+		HashMap<Integer, Integer> Result = time(arr);
+
 		for (int i = 0; i < standard.length; i++) {
-			Result=ans(Result,standard[i]);
+			Result = ans(Result, standard[i]);
 		}
-		
+		System.out.println(Result);
+		System.out.println(result1.substring(0,2)+":"+result1.substring(2,4));
 	}
 
-		
+	public static HashMap<Integer, Integer> time(int[] arr) {
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[i], map.get(arr[i] + 1));
+			} else
+				map.put(arr[i], 1);
 
-		public static HashMap<Integer, Integer> time(int[] arr) {
-			HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-			for (int i = 0; i < arr.length; i++) {
-				if (map.containsKey(arr[i])) {
-					map.put(arr[i], map.get(arr[i]+1));
-				}
-				else
-					map.put(arr[i], 1);
-			  
-			}
-			System.out.println(map);
-			return map;
 		}
+		System.out.println(map);
+		return map;
+	}
+
+	private static HashMap<Integer, Integer> ans(HashMap<Integer, Integer> time, int limit) {
+		for (int i = limit; i > 0; i--) {
+			if (time.containsKey(i) && (time.get(i)>0)) {
+				result1 += Integer.toString(i);// Check static properties
+				time.put(i, time.get(i) - 1);
+				break;
+			}
+		}
+
 		
-		private static HashMap<Integer, Integer> ans(HashMap<Integer, Integer> time, int limit) {
-			for (int i = limit; i > 0; i--) {
-				
-			}
- 
-			
-			return time;
-		}
+		return time;
+	}
 }
